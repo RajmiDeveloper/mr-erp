8f388ed510b3db7126446ed1372c3ae790c0a59e
RajmiDeveloper <matiasgrajmilevich@gmail.com>
2026-07-12 11:29:07 -0300

    real first commit

---
+++ src/main/java/com/matias/mrerp/MrErpApplication.java
@@ -0,0 +1,13 @@
+package com.matias.mrerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrErpApplication.class, args);
+	}
+
+}
