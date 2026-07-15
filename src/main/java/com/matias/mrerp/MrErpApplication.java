da88a83e15c7b23413a5ea5255a67ee80c885c58
matias rajmilevich <matiasgrajmilevich@gmail.com>
2026-07-15 08:07:44 -0300

    changes to boot in windows

--- src/main/java/com/matias/mrerp/MrErpApplication.java
+++ src/main/java/com/matias/mrerp/MrErpApplication.java
@@ -1,10 +1,16 @@
 package com.matias.mrerp;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MrErpApplication {
+
+	static {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MrErpApplication.class, args);
