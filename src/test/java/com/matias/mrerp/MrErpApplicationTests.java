3a0f6f9e7761d1412c7c81188a2062a83a195b69
RajmiDeveloper <matiasgrajmilevich@gmail.com>
2026-07-16 16:23:06 -0300

    fix: restore src

---
+++ src/test/java/com/matias/mrerp/MrErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.mrerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
